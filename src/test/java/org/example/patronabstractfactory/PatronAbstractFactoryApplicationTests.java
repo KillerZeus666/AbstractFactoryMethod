c14c938405607f1afff0a0cf79c2c78db73c7d9a
KillerZeus666 <katheryng2017@gmail.com>
2025-02-17 13:02:53 -0500

    First Commit

---
+++ src/test/java/org/example/patronabstractfactory/PatronAbstractFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.patronabstractfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronAbstractFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
